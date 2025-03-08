61335c9ebc21c73a4cbf5ab1c843fd8662a080ae
Nabillera <vatcharadzenini@gmail.com>
2025-03-08 14:50:53 +0400

    modified tab

--- src/components/MainDisplay.jsx
+++ src/components/MainDisplay.jsx
@@ -1,17 +1,19 @@
 import NewProjectForm from "./NewProjectForm";
+import Folder from "../assets/folder.png";
 
 export default function MainDisplay({ activePage, onChangePage, onSave }) {
   return (
     <div
       id="main-display"
-      className="relative pt-[70px] justify-center flex h-[90%] w-[100%]"
+      className="relative flex h-[90%] w-[100%] justify-center pt-[70px]"
     >
       {activePage == "main" ? (
         <div className="flex flex-col items-center">
-          <h2 className="text-[26px] font-bold text-neutral-600">
+          <img className="mt-[50px] w-[120px]" src={Folder} />
+          <h2 className="mt-[15px] mb-[10px] text-[32px] font-bold text-neutral-600">
             No Project Selected
           </h2>
-          <p className="text-[18px] text-neutral-400">
+          <p className="text-[24px] text-neutral-400">
             Select a project or get started with a new one
           </p>
         </div>
